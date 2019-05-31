93133d143d3efa287c66192bc933e6c5c6207b75
temp <temp@admins-MacBook-Pro.local>
2019-05-31 10:11:52 +0200

    added more applications to mac setup script and more system preferences changes

--- mac.sh
+++ mac.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+xcode-select --install
 
 read -p "Install Brew $foo? [yn]" answer
 if [[ $answer = y ]] ; then
@@ -35,17 +36,32 @@ if [[ $answer = y ]] ; then
   curl https://rclone.org/install.sh | sudo bash
 fi
 
-read -p "Installing gui Applications, chrome, vlc, libreoffice, visual-studio-code, filezilla, shift"
+read -p "Installing gui Applications, chrome, vlc, libreoffice, visual-studio-code, filezilla, shift, gimp, the-unarchiver, continue? y or n [yn]" answer
+if [[ $answer = y ]] ; then
+  brew cask install google-chrome
+  brew cask install vlc 
+  brew cask install libreoffice
+  brew cask install visual-studio-code
+  brew cask install cyberduck
+  brew cask install the-unarchiver
+  brew cask install gimp
+  brew cask install skype
+  brew cask install steam
+  brew cask install virtualbox
+  brew cask install nano
+  brew cask install vim
+  brew cask install htop
+  brew cask install nmap
+  brew cask install unzip
+  brew cask install 7zip
+  brew cask install disk-inventory-x
+  brew install node
+  brew install tree
 
-brew cask install google-chrome
+fi
 
-brew cask install vlc 
-
-brew cask install libreoffice
-
-brew cask install visual-studio-code
-
-brew cask install filezilla
+read -p "cleaning up brew"
+brew update && brew upgrade && brew cleanup && brew cask cleanup
 
 read -p "Download shift? y or n [yn]" answer
 if [[ $answer = y ]] ; then 
@@ -58,3 +74,20 @@ sudo sudo hdiutil attach ~/Downloads/shift-mac.dmg
 
 sudo cp -R /Volumes/shift-mac/shift.app /Applications
 
+read -p "Setting up system preferences"
+
+echo "Reboot to a clean state set to true"
+defaults write com.apple.systempreferences NSQuitAlwaysKeepWindows -bool false
+echo "Ignore quarentine of unknown apps"
+defaults write com.apple.LaunchServices LSQuarantine -bool false
+echo "Invert scrolling direction"
+defaults write NSGlobalDomain com.apple.swipescrolldirection -bool false
+echo "Unhide "~/Library" folder."
+chflags nohidden ~/Library
+echo "Allow selecting text in Quick Look."
+defaults write com.apple.finder QLEnableTextSelection -bool true
+echo "Show path in Finder"
+defaults write com.apple.finder ShowPathbar -bool true
+defaults write com.apple.finder _FXShowPosixPathInTitle -bool true
+echo "Show extensions in Finder"
+defaults write NSGlobalDomain AppleShowAllExtensions -bool true
